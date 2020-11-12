aa52c9bdd3fa87410f80fcbb65a1f0d352d813e0
Aruzhan-02 <aruzhansabit2@gmail.com>
2020-11-12 11:35:07 +0600

    mini calc finished

--- app/src/main/java/com/example/mini_calculator/MainActivity.java
+++ app/src/main/java/com/example/mini_calculator/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.mini_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText number1, number2;
     Button btnPlus, btnMinus;
     Button btnMulti, btnDivide;
@@ -27,5 +28,53 @@ public class MainActivity extends AppCompatActivity {
         btnDivide = findViewById(R.id.button_divide);
 
         result = findViewById(R.id.tv_result);
+
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnMulti.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int res = 0;
+        if (view.getId() == R.id.button_plus) {
+            res = Integer.parseInt(number1.getText().toString()) + Integer.parseInt(number2.getText().toString());
+        } else if (view.getId() == R.id.button_minus) {
+            res = Integer.parseInt(number1.getText().toString()) - Integer.parseInt(number2.getText().toString());
+        } else if (view.getId() == R.id.button_multi) {
+            res = Integer.parseInt(number1.getText().toString()) * Integer.parseInt(number2.getText().toString());
+        } else if (view.getId() == R.id.button_divide) {
+            res = Integer.parseInt(number1.getText().toString()) / Integer.parseInt(number2.getText().toString());
+
+        }
+
+        result.setText("Result: " + res);
+    }
+
+    public void BtnPlusClicked(View view) {
+        int res = Integer.parseInt(number1.getText().toString()) + Integer.parseInt(number2.getText().toString());
+
+        result.setText("Result: " + res);
+    }
+
+    public void BtnMinusClicked(View view) {
+        int res = Integer.parseInt(number1.getText().toString()) - Integer.parseInt(number2.getText().toString());
+
+        result.setText("Result: " + res);
+    }
+
+    public void BtnMultiClicked(View view) {
+        int res = Integer.parseInt(number1.getText().toString()) * Integer.parseInt(number2.getText().toString());
+
+        result.setText("Result: " + res);
+    }
+
+    public void BtnDivideClicked(View view) {
+        int res = Integer.parseInt(number1.getText().toString()) + Integer.parseInt(number2.getText().toString());
+
+        result.setText("Result: " + res);
     }
 }
+
